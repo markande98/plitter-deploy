37cc206a109b4bcf996a725918eaa9c02e58e672
markande98 <gauravbpit1998@gmail.com>
2023-08-18 13:42:42 +0530

    created user profile

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import UserBio from "@/components/users/UserBio";
 import UserHero from "@/components/users/UserHero";
 import useUser from "@/hooks/useUser";
 import { useRouter } from "next/router";
@@ -21,6 +22,7 @@ const UserView = () => {
     <>
       <Header label={fetchedUser?.name} showBackArrow />
       <UserHero userId={userId as string} />
+      <UserBio userId={userId as string} />
     </>
   );
 };

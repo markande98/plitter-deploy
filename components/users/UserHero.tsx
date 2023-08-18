acaaabb7c3484951e0162134ae83eb322bd310d9
markande98 <gauravbpit1998@gmail.com>
2023-08-18 13:23:07 +0530

    started working with profile page

---
+++ components/users/UserHero.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+import useUser from "@/hooks/useUser";
+
+import Avatar from '../Avatar';
+
+
+interface UserHeroProps{
+    userId : string;
+}
+
+const UserHero: React.FC<UserHeroProps> = ({ userId }) => {
+    const { data : fetchedUser} = useUser(userId);
+    return (
+        <div className='
+            bg-neutral-700 h-44 relative
+        '>
+
+
+        </div>
+    )
+}
+export default UserHero;
---
+++ pages/users/[userId].tsx
@@ -0,0 +1,28 @@
+import Header from "@/components/Header";
+import UserHero from "@/components/users/UserHero";
+import useUser from "@/hooks/useUser";
+import { useRouter } from "next/router";
+import { ClipLoader } from "react-spinners";
+
+const UserView = () => {
+    const router = useRouter();
+    const { userId } = router.query;
+
+    const { data : fetchedUser, isLoading } = useUser(userId as string);
+
+    if(isLoading || !fetchedUser){
+        return (
+            <div className="flex justify-center items-center h-full">
+                <ClipLoader color="lightblue" size={80} />
+            </div>
+        )
+    }
+  return (
+    <>
+      <Header label={fetchedUser?.name} showBackArrow />
+      <UserHero userId={userId as string} />
+    </>
+  );
+};
+
+export default UserView;

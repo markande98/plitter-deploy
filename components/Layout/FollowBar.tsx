da74c86560cbb3a30fe563f468669e873cdae8c3
markande98 <gauravbpit1998@gmail.com>
2023-08-18 17:16:02 +0530

    postfeed added

--- components/Layout/FollowBar.tsx
+++ components/Layout/FollowBar.tsx
@@ -12,15 +12,15 @@ const FollowBar = () => {
     <div className="px-6 py-4 hidden lg:block">
       <div className="bg-neutral-800 rounded-xl p-4">
         <h2 className="text-white font-semibold text-xl">Who to follow</h2>
-        <div className="flex flex-row mt-4 gap-6">
+        <div className="flex flex-col mt-4 gap-6">
           {users.map((user: Record<string, any>) => (
-            <>
+            <div className="flex flex-row">
               <Avatar userId={user.id} />
-              <div className="flex flex-col">
+              <div className="flex flex-col ml-4">
                 <p className="text-white font-semibold text-sm">{user.name}</p>
                 <p className="text-neutral-400 text-sm">@{user.username}</p>
               </div>
-            </>
+            </div>
           ))}
         </div>
       </div>
--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
 import UserBio from "@/components/users/UserBio";
 import UserHero from "@/components/users/UserHero";
 import useUser from "@/hooks/useUser";
@@ -6,23 +7,24 @@ import { useRouter } from "next/router";
 import { ClipLoader } from "react-spinners";
 
 const UserView = () => {
-    const router = useRouter();
-    const { userId } = router.query;
+  const router = useRouter();
+  const { userId } = router.query;
 
-    const { data : fetchedUser, isLoading } = useUser(userId as string);
+  const { data: fetchedUser, isLoading } = useUser(userId as string);
 
-    if(isLoading || !fetchedUser){
-        return (
-            <div className="flex justify-center items-center h-full">
-                <ClipLoader color="lightblue" size={80} />
-            </div>
-        )
-    }
+  if (isLoading || !fetchedUser) {
+    return (
+      <div className="flex justify-center items-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    );
+  }
   return (
     <>
       <Header label={fetchedUser?.name} showBackArrow />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
+      <PostFeed userId={userId as string} />
     </>
   );
 };

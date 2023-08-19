23c282e20da28e90f2c89494281c5699805fb3c5
markande98 <gauravbpit1998@gmail.com>
2023-08-19 15:52:11 +0530

    implemented comment

--- components/Form.tsx
+++ components/Form.tsx
@@ -7,6 +7,7 @@ import { useCallback, useState } from "react";
 import { toast } from "react-hot-toast";
 import Button from "./Button";
 import Avatar from "./Avatar";
+import usePost from "@/hooks/usePost";
 
 interface FormProps {
   placeholder: string;
@@ -20,7 +21,9 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
 
   const { data: currentUser } = useCurrentUser();
 
-  const { mutate: mutatePosts } = usePosts(postId as string);
+  const { mutate: mutatePosts } = usePosts();
+
+  const { mutate: mutatePost } = usePost(postId as string);
 
   const [body, setBody] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -29,18 +32,20 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     try {
       setIsLoading(true);
 
-      await axios.post("/api/posts", { body });
+      const url = isComment ? `/api/comments?postId=${postId}` : "/api/posts";
+      await axios.post(url, { body });
 
       toast.success("Tweet Created");
 
       setBody("");
       mutatePosts();
+      mutatePost();
     } catch (error) {
       toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, isComment, postId, mutatePost]);
 
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
@@ -78,7 +83,11 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
             "
             />
             <div className="mt-4 flex flex-row justify-end">
-              <Button disabled={isLoading || !body} onClick={onSubmit} label="Tweet" />
+              <Button
+                disabled={isLoading || !body}
+                onClick={onSubmit}
+                label="Tweet"
+              />
             </div>
           </div>
         </div>

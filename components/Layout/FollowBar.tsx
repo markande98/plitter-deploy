b496f17aaf651386f056f0a794a23d5203664557
Gaurav Tiwari <gauravbpit1998@gmail.com>
2023-08-28 18:01:27 +0530

    Update FollowBar.tsx

--- components/Layout/FollowBar.tsx
+++ components/Layout/FollowBar.tsx
@@ -14,8 +14,8 @@ const FollowBar = () => {
         <h2 className="text-white font-semibold text-xl">Who to follow</h2>
         <div className="flex flex-col mt-4 gap-6">
           {users.map((user: Record<string, any>) => (
-            <div className="flex flex-row">
-              <Avatar userId={user.id} />
+            <div key={user.id} className="flex flex-row">
+              <Avatar userId={user.id}/>
               <div className="flex flex-col ml-4">
                 <p className="text-white font-semibold text-sm">{user.name}</p>
                 <p className="text-neutral-400 text-sm">@{user.username}</p>
